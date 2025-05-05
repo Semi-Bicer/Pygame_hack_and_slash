242aaa4e67ed538588b4136552b7d0430bbf7612
Maide-karamanoglu <maidekaramanoglu@gmail.com>
2025-05-05 18:34:00 +0300

    Can barı ve menü eklendi

--- pygame_hack&slash/firstGame.py
+++ pygame_hack&slash/firstGame.py
@@ -18,6 +18,8 @@ width = 10
 height = 10
 vel = 20
 dashVel = 40
+health = 100
+maxHealth = 100
 ## rotation of the player
 left = False
 right = False
@@ -25,6 +27,27 @@ leftIdle = False
 rightIdle = False
 walkCount = 0
 idleCount = 0
+
+game_active = False
+
+font = pygame.font.SysFont("comicsans", 30)
+
+def draw_start_button():
+    btn_rect = pygame.Rect(screenWidth//2 - 100, screenHeight//2 - 25, 200,50)
+    pygame.draw.rect(win, (0, 200, 0), btn_rect)
+    text = font.render("Start", True, (255, 255, 255))
+    win.blit(text, (btn_rect.x + 50, btn_rect.y + 4))
+    return btn_rect
+
+def draw_health_bar(surface, x ,y,health, maxHealth ):
+    barWidth = 100
+    barHeight = 10
+    fill = (health / maxHealth) * barWidth
+    border_rect = pygame.Rect(x, y -20, barWidth, barHeight)
+    fill_rect = pygame.Rect(x, y -20, fill, barHeight)
+    pygame.draw.rect(surface, (255, 0, 0), fill_rect)
+    pygame.draw.rect(surface, (255, 255, 255), border_rect, 2)
+
 
 def load_sprite_sheet(sheet_path, sprite_width, sprite_height, num_sprites):
     sheet = pygame.image.load(sheet_path)
@@ -68,7 +91,13 @@ def redrawGameWindow():
             win.blit(charIdle[idleCount], (x, y))
             idleCount = (idleCount + 1) % len(charIdle)
 
-    
+
+    draw_health_bar(win, x, y, health, maxHealth)
+
+    if health <= 0:
+        gameOverText = font.render("GAME OVER", True, (255, 255, 255))
+        win.blit(gameOverText, ((screenWidth // 2 -gameOverText.get_width() // 2) , 300))
+
     
     pygame.display.update()
 
@@ -80,6 +109,17 @@ while run:
     for event in pygame.event.get():
         if event.type ==  pygame.QUIT:
             run = False
+        if not game_active and event.type == pygame.MOUSEBUTTONDOWN:
+            mousePos = pygame.mouse.get_pos()
+            if draw_start_button().collidepoint(mousePos):
+                game_active = True
+
+    if not game_active:
+        win.fill((0, 0, 0))
+        draw_start_button()
+        pygame.display.update()
+        continue
+
 
 
     keys = pygame.key.get_pressed()
@@ -127,6 +167,12 @@ while run:
         if keys[pygame.K_s]:
             y += dashVel
         isDashing = False
+
+    if keys[pygame.K_SPACE] and health > 0:
+            health -= 10
+            pygame.time.delay(100)
+
+
 
     redrawGameWindow()
 
